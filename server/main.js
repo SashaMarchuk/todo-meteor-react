96381ced9296488c4e9e3a9e465df59c957b3eb2
Sasha Marchuk <Sasha.Marchuk.email@gmail.com>
2022-07-14 17:56:17 +0300

    Add Login with github

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { Tasks } from "../imports/api/Tasks/tasks";
 import { Accounts } from "meteor/accounts-base";
+import { ServiceConfiguration } from "meteor/service-configuration";
 
 const insertTask = (taskText, user) =>
   Tasks.insert({
@@ -34,3 +35,14 @@ Meteor.startup(() => {
     ].forEach((taskText) => insertTask(taskText, user));
   }
 });
+
+ServiceConfiguration.configurations.upsert(
+  { service: "github" },
+  {
+    $set: {
+      loginStyle: "popup",
+      clientId: "6c09d20a4d5f8288afb3", // insert your clientId here
+      secret: "67a9e34208470a04547b1932f153780b2d085a65", // insert your secret here
+    },
+  }
+);
